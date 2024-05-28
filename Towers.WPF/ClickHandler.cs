a8736497aa42500a64664f51b901d030b9bd0fe2
GET Academy <terje@kolderup.net>
2024-05-28 11:45:05 +0200

    x

---
+++ Towers.WPF/ClickHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Towers.WPF
+{
+    class ClickHandler
+    {
+        private int _from;
+        private int _to;
+
+        public ClickHandler(int from, int to)
+        {
+            _to = to;
+            _from = from;
+        }
+    }
+}
---
+++ Towers.WPF/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Towers.Logic;
+
+namespace Towers.WPF
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var game = new Game();
+
+            var app = new Application();
+            var window = new Window();
+            var gamePanel = new GamePanel(game);
+            window.Content = gamePanel.Panel;
+            app.Run(window);
+        }
+    }
+}
